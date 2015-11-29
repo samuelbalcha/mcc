a39b5efefe9b3033a76b9dcddf3aef4399dacfd1
lulitw <lulitw@gmail.com>
2015-11-29 22:27:19 +0200

    -Added calendar sync

--- andorid/Whatsout/app/src/main/java/com/appdev/sammyb/whatsout/models/EventModel.java
+++ andorid/Whatsout/app/src/main/java/com/appdev/sammyb/whatsout/models/EventModel.java
@@ -5,9 +5,13 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.DateTimeFormatterBuilder;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -19,6 +23,11 @@ public class EventModel implements Serializable {
     public String title;
     public String description;
     public String date;
+    public String gid;
+    public DateTime sdate;
+    public DateTime edate;
+    public String location;
+
     //private static final long serialVersionUID = -111312112;
     private static ArrayList<EventModel>  Events = new ArrayList<EventModel>();
 
@@ -29,16 +38,29 @@ public class EventModel implements Serializable {
             this.id = object.getString("_id");
             this.title = object.getString("title");
             this.description = object.getString("description");
-           // this.date = object.getString("date") + " - " + object.getString("dateEnd");
+            this.gid = object.has("gid") ? object.getString("gid") : null;
+            this.location = object.has("location") ? object.getString("location") : "";
+            this.sdate = object.has("date") ? new DateTime(object.getString("date")) : new DateTime();
+            this.edate = object.has("dateEnd") ? new DateTime(object.getString("dateEnd")) : new DateTime();
 
-            DateFormat format = new SimpleDateFormat("MMMM dd, yyyy  hh:mm aaa", Locale.ENGLISH);
-            try {
-                this.date = format.parse(object.getString("date")).toString() + " - "
-                        + format.parse(object.getString("dateEnd")).toString();
 
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
+            DateTimeFormatter fmt = new DateTimeFormatterBuilder()
+                    .appendMonthOfYearShortText()
+                    .appendLiteral(' ')
+                    .appendDayOfMonth(1)
+                    .appendLiteral(',')
+                    .appendLiteral(' ')
+                    .appendYearOfCentury(2,4)
+                    .appendLiteral(' ')
+                    .appendHourOfDay(2)
+                    .appendLiteral(':')
+                    .appendMinuteOfHour(2)
+                    .toFormatter();
+
+            DateTime sdate = new DateTime(object.getString("date"));
+            DateTime edate = new DateTime(object.getString("dateEnd"));
+
+            this.date =  sdate.toString(fmt) + " - " + edate.toString(fmt);
 
         } catch (JSONException e) {
             e.printStackTrace();;
